e2d87e59a8ed1f28aadf93fca0eaeedd0f01ff4e
贺创 <2236402130@qq.com>
2023-07-24 11:41:42 +0800

    nimbleDragResize init

--- Audio/musicNameList.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const musicNameList: string[];
-export declare function getMusicName(index: any): any;
-export declare function getMusicList(): any[];
--- Audio/tool.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function getTimeByStamp(timeStamp: number): string;
