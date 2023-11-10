0c765a3db88e177d63db0426b278948d4af6a169
贺创 <2236402130@qq.com>
2023-11-10 14:57:35 +0800

    区域选择：优化容器元素获取

--- magicDrag/plugins/regionalSelection.ts
+++ magicDrag/plugins/regionalSelection.ts
@@ -1,6 +1,5 @@
 import { Plugin } from '../functions'
-import { nextTick } from '@vue/runtime-core'
-import {getElement, numberToStringSize, setStyle} from '../utils/tools'
+import { getElement, numberToStringSize, setStyle } from '../utils/tools'
 
 /*
 * 获取容器元素可以在按下鼠标的时候获取，这个时候DOM必然已经插入完毕
@@ -11,29 +10,32 @@ class RegionalSelection implements Plugin {
   private regionalEl = null
   private startCoordinate = { x: 0, y: 0 }
   private isPress = false
-  constructor(private container: string | HTMLElement) {
+  private containerEl
+  constructor(private containerSelector: string) {
     this.name = 'regionalSelection'
     this.bindMouseDown = this._mousedown.bind(this)
     this.bindMouseMove = this._mouseMove.bind(this)
     this.bindMouseUp = this._mouseup.bind(this)
   }
   init() {
-    nextTick(this.readyContainer.bind(this))
-  }
-  unbind() {
-    this.container = null
-    window.removeEventListener('mousedown', this.bindMouseDown)
-    window.removeEventListener('mousemove', this.bindMouseMove)
-    window.removeEventListener('mouseup', this.bindMouseUp)
-  }
-  readyContainer() {
-    this.container = getElement(this.container)
     window.addEventListener('mousedown', this.bindMouseDown)
     window.addEventListener('mousemove', this.bindMouseMove)
     window.addEventListener('mouseup', this.bindMouseUp)
   }
+  unbind() {
+    this.containerSelector = null
+    window.removeEventListener('mousedown', this.bindMouseDown)
+    window.removeEventListener('mousemove', this.bindMouseMove)
+    window.removeEventListener('mouseup', this.bindMouseUp)
+  }
+  getContainer() {
+    if (this.containerEl) return this.containerEl
+    this.containerEl = getElement(this.containerSelector)
+    return this.containerEl
+  }
   _mousedown(event) {
-    if (event.target !== this.container) return
+    this.getContainer()
+    if (event.target !== this.containerEl) return
     this.isPress = true
     if (!this.regionalEl) this.regionalEl = document.createElement('div')
     this.startCoordinate.x = event.pageX
@@ -41,10 +43,7 @@ class RegionalSelection implements Plugin {
     const regionalStyle = {
       position: 'absolute',
       border: '1px solid aqua',
-      zIndex: '88888',
-      width: '0',
-      height: '0',
-      display: 'block'
+      zIndex: '88888'
     }
     setStyle(this.regionalEl, regionalStyle)
     document.body.appendChild(this.regionalEl)
