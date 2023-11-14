bbebd6bba07cefa5437e206355f6be37533089da
贺创 <2236402130@qq.com>
2023-11-14 15:13:50 +0800

    单元测试（参数检查辅助功能完善）

--- magicDrag/common/warningAssist.ts
+++ magicDrag/common/warningAssist.ts
@@ -25,10 +25,11 @@ export function checkParameterValue(options: MagicDragOptions) {
   checkCustomStyle(options.customStyle)
 }
 export function checkOptionSize(options: Partial<MagicDragOptions>) {
-  baseWarnTips(errorSize(),`The maximum value cannot be less than the minimum value, and the maximum and minimum values have been replaced with each other`)
-  replaceExtremum()
-  baseWarnTips(lessThanMinimum(), 'The initial size cannot be less than the minimum size')
-  baseWarnTips(greaterThanMaximum(), 'The initial size cannot be greater than the maximum size')
+  baseWarnTips(errorSize(),`The maximum value cannot be less than the minimum value, and the maximum and minimum values have been replaced with each other.`)
+  // 最大最小值无效时替换最大最小值
+  fixMaxAndMin()
+  baseWarnTips(lessThanMinimum(), 'The initial size cannot be less than the minimum size.')
+  baseWarnTips(greaterThanMaximum(), 'The initial size cannot be greater than the maximum size.')
   function lessThanMinimum() {
     return options.initialInfo.height < options.minHeight || options.initialInfo.width < options.minWidth
   }
@@ -38,7 +39,7 @@ export function checkOptionSize(options: Partial<MagicDragOptions>) {
   function errorSize() {
     return options.minWidth > options.maxWidth || options.minHeight > options.maxHeight
   }
-  function replaceExtremum() {
+  function fixMaxAndMin() {
     const { minWidth, maxWidth, minHeight, maxHeight } = options
     if (minWidth > maxWidth) {
       options.minWidth = maxWidth
@@ -51,45 +52,70 @@ export function checkOptionSize(options: Partial<MagicDragOptions>) {
   }
 }
 function checkOptionPosition(initialInfo: MagicDragOptions['initialInfo']) {
-  baseWarnTips(posBeNegative(), 'It is not recommended that the initial location information be set to a negative value')
+  baseWarnTips(posBeNegative(), 'It is not recommended that the initial location information be set to a negative value.')
+  fixInitialPos()
+  function fixInitialPos() {
+    initialInfo.top = initialInfo.top > 0 ? initialInfo.top : 0
+    initialInfo.left = initialInfo.left > 0 ? initialInfo.left : 0
+  }
 
+  fixInitialPos()
   function posBeNegative() {
     return initialInfo.left < 0 || initialInfo.top < 0
   }
 }
 function checkCustomStyle(styles: MagicDragOptions['customStyle']) {
   checkPointStyle(styles.pointStyle)
+  fixPointStyle(styles.pointStyle)
   checkRefLineStyle(styles.refLineStyle)
+  fixRefLineStyle(styles.refLineStyle)
   checkTipStyle(styles.tipStyle)
+  fixTipStyle(styles.tipStyle)
   function checkPointStyle(pointStyle: MagicDragOptions['customStyle']['pointStyle']) {
-    const unitRight = pointStyle?.width && !/^\d+px$/.test(pointStyle.width)
-      || pointStyle?.height && !/^\d+px$/.test(pointStyle.height)
-    baseErrorTips(unitRight,
-      'The dimensions of the outline points are supported only in px units')
-    baseErrorTips(pointStyle?.position && pointStyle.position !== 'absolute',
-      'Contour points must have absolute positioning turned on')
+    const unitRight = pointStyle?.width && /^\d+px$/.test(pointStyle.width)
+      && pointStyle?.height && /^\d+px$/.test(pointStyle.height)
+    baseErrorTips(!unitRight,
+      'The dimensions of the outline points are supported only in px units.')
+    baseWarnTips(pointStyle?.position && pointStyle.position !== 'absolute',
+      'PointStyle.position the expected value is absolute and is automatically modified.')
     baseWarnTips(pointStyle?.display && pointStyle.display !== 'none',
       `If you do not set the display property of the outline point to none,
-      redundant elements may be displayed after the initial rendering is complete`)
+      redundant elements may be displayed after the initial rendering is complete.`)
     baseWarnTips(pointStyle?.boxSizing && pointStyle.boxSizing !== 'border-box',
-      'If the contour point box-sizing property is not set to border-box, the contour point position may be shifted')
+      'If the contour point box-sizing property is not set to border-box, the contour point position may be shifted.')
   }
   function checkRefLineStyle(refLineStyle: MagicDragOptions['customStyle']['refLineStyle']) {
-    baseErrorTips(!isNullOrUndefined(refLineStyle?.width) || !isNullOrUndefined(refLineStyle?.height),
-      'Setting the guide size is not supported')
-    baseErrorTips(refLineStyle?.position && refLineStyle.position !== 'absolute',
-      'Contour points must have absolute positioning turned on')
+    baseWarnTips(!isNullOrUndefined(refLineStyle?.width) || !isNullOrUndefined(refLineStyle?.height),
+      'The guide size cannot be set. It has been moved and deleted.')
+    baseWarnTips(refLineStyle?.position && refLineStyle.position !== 'absolute',
+      'RefLineStyle.position the expected value is absolute and is automatically modified.')
     baseWarnTips(refLineStyle?.display && refLineStyle.display !== 'none',
       `If you do not set the display property of the outline point to none,
-      redundant elements may be displayed after the initial rendering is complete`)
+      redundant elements may be displayed after the initial rendering is complete.`)
   }
   function checkTipStyle(tipStyle: MagicDragOptions['customStyle']['tipStyle']) {
-    baseErrorTips(tipStyle?.position && tipStyle.position !== 'absolute',
-      'Contour points must have absolute positioning turned on')
+    baseWarnTips(tipStyle?.position && tipStyle.position !== 'absolute',
+      'TipStyle.position the expected value is absolute and is automatically modified.')
     baseWarnTips(tipStyle?.display && tipStyle.display !== 'none',
       `If you do not set the display property of the outline point to none,
-      redundant elements may be displayed after the initial rendering is complete`)
+      redundant elements may be displayed after the initial rendering is complete.`)
     baseWarnTips(tipStyle?.boxSizing && tipStyle.boxSizing !== 'border-box',
-      'If the contour point box-sizing property is not set to border-box, the contour point position may be shifted')
+      'If the contour point box-sizing property is not set to border-box, the contour point position may be shifted.')
+  }
+  function fixPointStyle(pointStyle: MagicDragOptions['customStyle']['pointStyle']) {
+    if (pointStyle?.position) pointStyle.position = 'absolute'
+    if (pointStyle?.display) pointStyle.display = 'none'
+    if (pointStyle?.boxSizing) pointStyle.boxSizing = 'border-box'
+  }
+  function fixRefLineStyle(refLineStyle: MagicDragOptions['customStyle']['refLineStyle']) {
+    if (refLineStyle?.width) delete refLineStyle.width
+    if (refLineStyle?.height) delete refLineStyle.height
+    if (refLineStyle?.position) refLineStyle.position = 'absolute'
+    if (refLineStyle?.display) refLineStyle.display = 'none'
+  }
+  function fixTipStyle(tipStyle: MagicDragOptions['customStyle']['tipStyle']) {
+    if (tipStyle?.position) tipStyle.position = 'absolute'
+    if (tipStyle?.display) tipStyle.display = 'none'
+    if (tipStyle?.boxSizing) tipStyle.boxSizing = 'border-box'
   }
 }
--- test/common/warningAssist.test.ts
+++ test/common/warningAssist.test.ts
@@ -3,7 +3,7 @@ import globalData from '../../magicDrag/common/globalData'
 import {mergeObject} from "../../magicDrag/utils/tools";
 
 describe('parameters check', function () {
-	it('Check that all field types of the parameter are the same', () => {
+	it('Check whether the field value types in the parameters meet the expectations.', () => {
 		const exampleParameter = {
 			str: 'string',
 			num: 5,
@@ -23,22 +23,25 @@ describe('parameters check', function () {
 			.toThrow(`The type of options.fun should be function, But the object type is passed in.`)
 	})
 
-	it('Check whether the parameter "initialInfo" field value is valid', () => {
+	it('Check whether the parameter "initialInfo" field value is valid.', () => {
 		const originWarn = console.warn
 		console.warn = jest.fn()
-		checkParameterValue(mergeObject(globalData.defaultOptions, { initialInfo: { left: -1, top: 5 } }))
+		const options = mergeObject(globalData.defaultOptions, { initialInfo: { left: 10, top: -20 } })
+		checkParameterValue(options)
 		expect(console.warn).toHaveBeenCalled()
-		expect(console.warn.mock.calls[0][0]).toContain('It is not recommended that the initial location information be set to a negative value')
+		expect(console.warn.mock.calls[0][0]).toContain('It is not recommended that the initial location information be set to a negative value.')
+		expect(options.initialInfo.left).toBe(10)
+		expect(options.initialInfo.top).toBe(0)
 		console.warn = originWarn
 	})
 
-	it('Exchange error field values', () => {
+	it('Exchange error field values.', () => {
 		const originWarn = console.warn
 		console.warn = jest.fn()
 		const options = mergeObject(globalData.defaultOptions, { minWidth: 20, maxWidth: 10, minHeight: 30, maxHeight: 20 })
 		checkParameterValue(options)
 		expect(console.warn).toHaveBeenCalled()
-		expect(console.warn.mock.calls[0][0]).toContain('The maximum value cannot be less than the minimum value, and the maximum and minimum values have been replaced with each other')
+		expect(console.warn.mock.calls[0][0]).toContain('The maximum value cannot be less than the minimum value, and the maximum and minimum values have been replaced with each other.')
 		expect(options.minWidth).toBe(10)
 		expect(options.maxWidth).toBe(20)
 		expect(options.minHeight).toBe(20)
@@ -46,14 +49,65 @@ describe('parameters check', function () {
 		console.warn = originWarn
 	})
 
-	it('Check the width and height related fields', () => {
+	it('Check the width and height related fields.', () => {
 		const originWarn = console.warn
 		console.warn = jest.fn()
 		checkParameterValue(mergeObject(globalData.defaultOptions, { initialInfo: { height: 50 }, minHeight: 100 }))
 		checkParameterValue(mergeObject(globalData.defaultOptions, { initialInfo: { height: 200 }, maxHeight: 100 }))
 		expect(console.warn).toHaveBeenCalledTimes(2)
-		expect(console.warn.mock.calls[0][0]).toContain('The initial size cannot be less than the minimum size')
-		expect(console.warn.mock.calls[1][0]).toContain('The initial size cannot be greater than the maximum size')
+		expect(console.warn.mock.calls[0][0]).toContain('The initial size cannot be less than the minimum size.')
+		expect(console.warn.mock.calls[1][0]).toContain('The initial size cannot be greater than the maximum size.')
+		console.warn = originWarn
+	})
+
+	it('Check that the value of the customStyle.pointStyle field is valid', () => {
+		const originWarn = console.warn
+		console.warn = jest.fn()
+		expect(() => checkParameterValue(mergeObject(globalData.defaultOptions, { customStyle: { pointStyle: { width: '20' } } })))
+			.toThrow('The dimensions of the outline points are supported only in px units.')
+		const options = mergeObject(globalData.defaultOptions, { customStyle: { pointStyle: { position: 'relative', display: 'block', boxSizing: 'content-box' } } })
+		checkParameterValue(options)
+		expect(console.warn).toHaveBeenCalledTimes(3)
+		expect(console.warn.mock.calls[0][0]).toContain('PointStyle.position the expected value is absolute and is automatically modified')
+		expect(console.warn.mock.calls[1][0]).toContain(`If you do not set the display property of the outline point to none,
+      redundant elements may be displayed after the initial rendering is complete.`)
+		expect(console.warn.mock.calls[2][0]).toContain(`If the contour point box-sizing property is not set to border-box, the contour point position may be shifted.`)
+		expect(options.customStyle.pointStyle.position).toBe('absolute')
+		expect(options.customStyle.pointStyle.display).toBe('none')
+		expect(options.customStyle.pointStyle.boxSizing).toBe('border-box')
+		console.warn = originWarn
+	})
+
+	it('Check that the value of the customStyle.refLineStyle field is valid', () => {
+		const originWarn = console.warn
+		console.warn = jest.fn()
+		const options = mergeObject(globalData.defaultOptions, { customStyle: { refLineStyle: { width: '1', position: 'relative', display: 'block' } } })
+		checkParameterValue(options)
+		expect(console.warn).toHaveBeenCalledTimes(3)
+		expect(console.warn.mock.calls[0][0]).toContain('The guide size cannot be set. It has been moved and deleted.')
+		expect(console.warn.mock.calls[1][0]).toContain('RefLineStyle.position the expected value is absolute and is automatically modified.')
+		expect(console.warn.mock.calls[2][0]).toContain(`If you do not set the display property of the outline point to none,
+      redundant elements may be displayed after the initial rendering is complete.`)
+		expect(options.customStyle.refLineStyle.width).toBeUndefined()
+		expect(options.customStyle.refLineStyle.height).toBeUndefined()
+		expect(options.customStyle.refLineStyle.position).toBe('absolute')
+		expect(options.customStyle.refLineStyle.display).toBe('none')
+		console.warn = originWarn
+	})
+
+	it('Check that the value of the customStyle.tipStyle field is valid', () => {
+		const originWarn = console.warn
+		console.warn = jest.fn()
+		const options = mergeObject(globalData.defaultOptions, { customStyle: { tipStyle: { position: 'relative', display: 'block', boxSizing: 'content-box' } } })
+		checkParameterValue(options)
+		expect(console.warn).toHaveBeenCalledTimes(3)
+		expect(console.warn.mock.calls[0][0]).toContain('TipStyle.position the expected value is absolute and is automatically modified.')
+		expect(console.warn.mock.calls[1][0]).toContain(`If you do not set the display property of the outline point to none,
+      redundant elements may be displayed after the initial rendering is complete.`)
+		expect(console.warn.mock.calls[2][0]).toContain(`If the contour point box-sizing property is not set to border-box, the contour point position may be shifted.`)
+		expect(options.customStyle.tipStyle.position).toBe('absolute')
+		expect(options.customStyle.tipStyle.display).toBe('none')
+		expect(options.customStyle.tipStyle.boxSizing).toBe('border-box')
 		console.warn = originWarn
 	})
 })
